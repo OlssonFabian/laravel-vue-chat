494dd64bbf41167f22db33c4799286f1da377145
Fabian Olsson <fabtasticdesign@live.se>
2020-09-09 14:21:29 +0200

    bootstrap now works (again)

---
+++ resources/js/components/ChatApp.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="chat-app">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('component mounted');
+        }
+    }
+</script>
